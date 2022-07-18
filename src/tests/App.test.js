c6a8e7e51c188e8a2092a8678bef2b2fffda5b25
Igor Sampaio <igorsamf@gmail.com>
2022-07-18 16:31:24 -0300

    requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWhithRouter';
+
+describe('Verifica se existe na aplicação o conjunto de links', () => {
+  test('', () => {
+    renderWithRouter(<App />);
+    const getLinkOne = screen.getByText(/Home/i);
+    expect(getLinkOne).toBeInTheDocument();
+
+    const getLinkTwo = screen.getByText(/About/i);
+    expect(getLinkTwo).toBeInTheDocument();
+
+    const getLinkThree = screen.getByText(/Favorite Pokémons/i);
+    expect(getLinkThree).toBeInTheDocument();
+  });
+});
