7ebeba0f8a51cc8335ddbe7508fe429408e09b43
Igor Sampaio <igorsamf@gmail.com>
2022-07-18 19:57:07 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import FavoritePokemons from '../pages/FavoritePokemons';
+import renderWithRouter from '../renderWhithRouter';
+
+describe('Verifica o componente FavoritePokemons', () => {
+  test('Teste se é exibida na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const getMessage = screen.getByText(/No favorite pokemon found/i);
+    expect(getMessage).toBeInTheDocument();
+  });
+});
