b3b1e7f4a2bd8cc639943e1f361ed9f31580cb42
Igor Sampaio <igorsamf@gmail.com>
2022-07-18 16:55:26 -0300

    requisito 1 incompleto

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from '../renderWhithRouter';
 
 describe('Verifica se existe na aplicação o conjunto de links', () => {
   test('', () => {
     renderWithRouter(<App />);
-    const getLinkOne = screen.getByText(/Home/i);
+    const getLinkOne = screen.getByRole('link', { name: /Home/ });
     expect(getLinkOne).toBeInTheDocument();
 
-    const getLinkTwo = screen.getByText(/About/i);
+    const getLinkTwo = screen.getByRole('link', { name: /About/ });
     expect(getLinkTwo).toBeInTheDocument();
 
-    const getLinkThree = screen.getByText(/Favorite Pokémons/i);
+    const getLinkThree = screen.getByRole('link', { name: /Favorite Pokémons/ });
     expect(getLinkThree).toBeInTheDocument();
+  });
+
+  test('Verifica se a aplicação é redirecionada para a página Home', () => {
+    const { history } = renderWithRouter(<App />);
+    const getLinkOne = screen.getByRole('link', { name: /Home/ });
+    userEvent.click(getLinkOne);
+    expect(history.location.pathname).toBe('/');
   });
 });
