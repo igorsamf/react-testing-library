08d7f7d331fd90d012d91cd622a8f4defc36d554
Igor Sampaio <igorsamf@gmail.com>
2022-07-18 17:13:01 -0300

    requisito 1 completo

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -23,4 +23,23 @@ describe('Verifica se existe na aplicação o conjunto de links', () => {
     userEvent.click(getLinkOne);
     expect(history.location.pathname).toBe('/');
   });
+  test('Verifica se a aplicação é redirecionada para a página About', () => {
+    const { history } = renderWithRouter(<App />);
+    const getLinkTwo = screen.getByRole('link', { name: /About/ });
+    userEvent.click(getLinkTwo);
+    expect(history.location.pathname).toBe('/about');
+  });
+  test('Verifica se a aplicação é redirecionada para a página Favorites', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkThree = screen.getByRole('link', { name: /Favorite Pokémons/ });
+    userEvent.click(linkThree);
+    expect(history.location.pathname).toBe('/favorites');
+  });
+  test('Verifica se a aplicação é redirecionada para a página NotFound', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/notFound');
+    expect(history.location.pathname).toBe('/notFound');
+    const getNotFound = screen.getByRole('heading', { name: /Page requested not found/ });
+    expect(getNotFound).toBeInTheDocument();
+  });
 });
